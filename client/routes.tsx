73cd3aaae8daaf15c86f1af90d4a15cf3d337859
Katie Davies <millerkatie1990@gmail.com>
2024-04-24 21:21:27 +1200

    add routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,18 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 import App from './components/App'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Learning from './components/Learning'
 
 
 const routes = createRoutesFromElements(
   <>
-    <Route path="/" element={<App/>} >
-    
-      
+    <Route path="/" element={<App/>}>
+      <Route path="about" element={<About/>}/>
+     <Route path="projects" element={<Projects/>}/>
+     <Route path="learning" element={<Learning/>}/>
+     <Route path="contact" element={<Contact/>}/>  
     </Route>
   </>,
 )
