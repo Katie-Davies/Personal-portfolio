8f5ac801288b6d7b8ca0ccfe3a1fc984eb282f6d
Katie Davies <millerkatie1990@gmail.com>
2024-04-25 11:04:43 +1200

    nav set up

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom"
+import Nav from "./Nav"
 
 
 
@@ -7,15 +8,18 @@ const App = () => {
   
   return (
     <>
+    <Nav/>
       <div className="landing-img flex content-center justify-start flex-wrap">
-        <div className="flex justify-center w-1/2">
+        <div className="flex flex-col justify-center w-1/2 content-center flex-wrap shrink">
           
-          <h1 className="mr-5 text-6xl">Katie Davies</h1>
+          <h1 className="mr-5 text-6xl mb-5">Katie Davies</h1>
+          <p>A Full-stack Developer with a passion for learning and teaching!</p>
           
       </div>
 
 
     </div>
+
     <Outlet/>
     </>
   )
--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,5 +1,14 @@
 function Nav () {
-  return <h1>Nav</h1>
+  return (
+  <div className="fixed flex justify-between w-full">
+    <h1 className="md:m-4 sm:m-2 lg:m-6">Katie Davies </h1>
+    <div className="flex justify-around">
+      <h1 className="md:m-4 sm:m-2 lg:m-6">About</h1>
+      <h1 className="md:m-4 sm:m-2 lg:m-6">Projects</h1>
+      <h1 className="md:m-4 sm:m-2 lg:m-6">Learning</h1>
+      <h1 className="md:m-4 sm:m-2 lg:m-6">Contact</h1>
+    </div>
+    </div>)
 }
 
 export default Nav
