6dbb46296a2cd1087fd4ffb7498159409867a4e4
Katie Davies <millerkatie1990@gmail.com>
2024-04-26 16:23:08 +1200

    add contact links

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -1,9 +1,25 @@
 function Contact() {
   return (
-    <div className="contact-img">
-      <h1 className="text-base sm:text-lg md:text-2xl lg:text-4xl mb-5 ">
+    <div className="contact-img flex justify-center flex-col content-center flex-wrap">
+      <h1 className="text-base sm:text-lg md:text-2xl lg:text-4xl mb-20 text-center">
         Contact
       </h1>
+      <div className="flex justify-around w-1/3">
+        <a href="https://www.linkedin.com/in/katie-davies-36351b275/">
+          <img
+            src="client/assets/LI-Logo.png"
+            alt="linked in logo"
+            className=" h-10 w-24"
+          ></img>
+        </a>
+        <a href="https://github.com/Katie-Davies">
+          <img
+            src="client/assets/GitHub_Logo.png"
+            alt="linked in logo"
+            className=" h-10 w-20"
+          ></img>
+        </a>
+      </div>
     </div>
   )
 }
